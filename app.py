a8a5a80db54a1eaec7fbc6a33a1752e9606127d3
Aditk23 <adit.k2398@gmail.com>
2021-08-01 17:19:08 +0530

    Styling

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 model = joblib.load('diabetes_model.pkl')
 @app.route('/')
 def welcome():
-    return render_template('input.html')
+    return render_template('index.html')
 
 @app.route('/input',methods=['POST'])
 def inputs():
